59a068b6b9584f9242829152eb29d6ba9350b1c8
Nick Bradshaw <nickalasb@gmail.com>
2019-02-09 14:17:56 -0800

    SMP-7: Pulled in android x material components to utilize collapsing toolbar layout

--- app/src/main/java/com/zonkey/simplemealplanner/activity/RecipeDetailActivity.kt
+++ app/src/main/java/com/zonkey/simplemealplanner/activity/RecipeDetailActivity.kt
@@ -10,10 +10,11 @@ import com.zonkey.simplemealplanner.R
 import com.zonkey.simplemealplanner.R.string
 import com.zonkey.simplemealplanner.adapter.FULL_RECIPE
 import com.zonkey.simplemealplanner.model.edamam.Recipe
-import kotlinx.android.synthetic.main.activity_recipe_detail.recipe_detail_Image
-import kotlinx.android.synthetic.main.activity_recipe_detail.recipe_detail_calories
-import kotlinx.android.synthetic.main.activity_recipe_detail.recipe_detail_title
-import kotlinx.android.synthetic.main.activity_recipe_detail.recipee_detail_servings
+import kotlinx.android.synthetic.main.activity_recipe_detail.detail_collapsing_toolbar
+import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_calories
+import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_image
+import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_servings
+import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_title
 import java.io.Serializable
 import kotlin.math.roundToInt
 
@@ -29,17 +30,18 @@ class RecipeDetailActivity : AppCompatActivity(), Serializable {
 
     val recipe = Gson().fromJson(intent.getStringExtra(FULL_RECIPE), Recipe::class.java)
 
-    recipe_detail_title.text = recipe.label
+    detail_recipe_title.text = recipe.label
+    detail_collapsing_toolbar.title = recipe.label
 
     val servingSizeText = "${getString(string.recipe_details_serving)}${recipe.yield}"
-    recipee_detail_servings.text = servingSizeText
+    detail_recipe_servings.text = servingSizeText
 
     val calsPerServing = (recipe.calories / recipe.yield).roundToInt()
     val caloriesText = "${getString(string.recipe_detail_calories)} $calsPerServing"
-    recipe_detail_calories.text = caloriesText
+    detail_recipe_calories.text = caloriesText
 
     Glide.with(this)
         .load(recipe.image)
-        .into(recipe_detail_Image)
+        .into(detail_recipe_image)
   }
 }
