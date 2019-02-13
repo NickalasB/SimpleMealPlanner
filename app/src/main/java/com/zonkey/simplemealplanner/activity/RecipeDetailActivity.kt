a58ef6ab27f4c5417eadf15834c948becc47bb84
Nick Bradshaw <nickalasb@gmail.com>
2019-02-12 22:55:36 -0800

    SMP-7: Add in custom tabs to launch out to full recipe

--- app/src/main/java/com/zonkey/simplemealplanner/activity/RecipeDetailActivity.kt
+++ app/src/main/java/com/zonkey/simplemealplanner/activity/RecipeDetailActivity.kt
@@ -3,6 +3,7 @@ package com.zonkey.simplemealplanner.activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Bundle
@@ -11,6 +12,7 @@ import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -21,11 +23,14 @@ import com.google.gson.Gson
 import com.zonkey.simplemealplanner.R
 import com.zonkey.simplemealplanner.adapter.FULL_RECIPE
 import com.zonkey.simplemealplanner.model.Recipe
+import com.zonkey.simplemealplanner.utils.createBitmapFromDrawableRes
 import kotlinx.android.synthetic.main.activity_recipe_detail.detail_collapsing_toolbar
 import kotlinx.android.synthetic.main.activity_recipe_detail.detail_ingredients_label
 import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_calories
 import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_image
+import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_link
 import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_servings
+import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_source
 import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_title
 import java.io.Serializable
 import kotlin.math.roundToInt
@@ -62,6 +67,24 @@ class RecipeDetailActivity : AppCompatActivity(), Serializable {
     detail_ingredients_label.text = getString(R.string.detail_ingredients_label)
 
     setUpIngredients(recipe)
+
+    val sourceText = "Source: ${recipe.source}"
+    detail_recipe_source.text = sourceText
+
+    setUpRecipeLinkButton(recipe)
+  }
+
+  private fun setUpRecipeLinkButton(recipe: Recipe) {
+    detail_recipe_link.setOnClickListener {
+      val customTabsBuilder = CustomTabsIntent.Builder()
+      val customTabsIntent = customTabsBuilder.build()
+      customTabsBuilder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+      val backArrowBitmap = createBitmapFromDrawableRes(this, R.drawable.ic_arrow_back_white_24dp)?.let { it }
+      backArrowBitmap?.let {
+        customTabsBuilder.setCloseButtonIcon(backArrowBitmap)
+      }
+      customTabsIntent.launchUrl(this, Uri.parse(recipe.url))
+    }
   }
 
   private fun loadRecipeImage(recipe: Recipe) {
@@ -97,7 +120,8 @@ class RecipeDetailActivity : AppCompatActivity(), Serializable {
       ingredientText.text = ingredientWithBullet
 
       val detailLayout: ViewGroup = findViewById(R.id.details_linear_layout)
-      detailLayout.addView(ingredientText, detailLayout.childCount,
+      // Ingredients are added at 2nd to last position in the layout
+      detailLayout.addView(ingredientText, 4,
           LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
     }
   }
--- app/src/main/java/com/zonkey/simplemealplanner/utils/AndroidUtils.kt
+++ app/src/main/java/com/zonkey/simplemealplanner/utils/AndroidUtils.kt
@@ -1,9 +1,36 @@
 package com.zonkey.simplemealplanner.utils
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
 
-inline fun <reified V : View> ViewGroup.inflate(@LayoutRes resId: Int, attach: Boolean = true) : V =
+inline fun <reified V : View> ViewGroup.inflate(@LayoutRes resId: Int, attach: Boolean = true): V =
     LayoutInflater.from(context).inflate(resId, this, attach) as V
+
+fun createBitmapFromDrawableRes(
+    context: Context,
+    resId: Int): Bitmap? {
+  val drawableResource = ContextCompat.getDrawable(context, resId)
+  drawableResource?.let { drawable ->
+    drawable.setBounds(
+        0,
+        0,
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight)
+
+    val bitMap = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888)
+
+    val canvas = Canvas(bitMap)
+    drawable.draw(canvas)
+    return bitMap
+  }
+  return null
+}
