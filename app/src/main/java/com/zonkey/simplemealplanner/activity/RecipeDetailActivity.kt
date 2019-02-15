cb31ca73a06b57eab913d89ac01e043a6d00ac67
Nick Bradshaw <nickalasb@gmail.com>
2019-02-14 22:41:09 -0800

    SMP-8: Extract RecipeDetails into custom widget

--- app/src/main/java/com/zonkey/simplemealplanner/activity/RecipeDetailActivity.kt
+++ app/src/main/java/com/zonkey/simplemealplanner/activity/RecipeDetailActivity.kt
@@ -3,17 +3,10 @@ package com.zonkey.simplemealplanner.activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -23,17 +16,9 @@ import com.google.gson.Gson
 import com.zonkey.simplemealplanner.R
 import com.zonkey.simplemealplanner.adapter.FULL_RECIPE
 import com.zonkey.simplemealplanner.model.Recipe
-import com.zonkey.simplemealplanner.utils.createBitmapFromDrawableRes
-import kotlinx.android.synthetic.main.activity_recipe_detail.detail_collapsing_toolbar
-import kotlinx.android.synthetic.main.activity_recipe_detail.detail_ingredients_label
-import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_calories
 import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_image
-import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_link
-import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_servings
-import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_source
-import kotlinx.android.synthetic.main.activity_recipe_detail.detail_recipe_title
+import kotlinx.android.synthetic.main.activity_recipe_detail.detailed_recipe_card_view
 import java.io.Serializable
-import kotlin.math.roundToInt
 
 
 class RecipeDetailActivity : AppCompatActivity(), Serializable {
@@ -53,38 +38,8 @@ class RecipeDetailActivity : AppCompatActivity(), Serializable {
     val recipe = Gson().fromJson(intent.getStringExtra(FULL_RECIPE), Recipe::class.java)
     loadRecipeImage(recipe)
 
-    detail_recipe_title.text = getString(R.string.recipe_detail_card_title)
-    detail_collapsing_toolbar.title = recipe.label
-    detail_collapsing_toolbar.setCollapsedTitleTextColor(ContextCompat.getColor(this, R.color.whiteText))
+    detailed_recipe_card_view.setRecipeDetailCardItems(recipe)
 
-    val servingSizeText = "${getString(R.string.recipe_details_serving)} ${recipe.yield}"
-    detail_recipe_servings.text = servingSizeText
-
-    val calsPerServing = (recipe.calories / recipe.yield).roundToInt()
-    val caloriesText = "${getString(R.string.recipe_detail_calories)} $calsPerServing"
-    detail_recipe_calories.text = caloriesText
-
-    detail_ingredients_label.text = getString(R.string.detail_ingredients_label)
-
-    setUpIngredients(recipe)
-
-    val sourceText = "Source: ${recipe.source}"
-    detail_recipe_source.text = sourceText
-
-    setUpRecipeLinkButton(recipe)
-  }
-
-  private fun setUpRecipeLinkButton(recipe: Recipe) {
-    detail_recipe_link.setOnClickListener {
-      val customTabsBuilder = CustomTabsIntent.Builder()
-      val customTabsIntent = customTabsBuilder.build()
-      customTabsBuilder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
-      val backArrowBitmap = createBitmapFromDrawableRes(this, R.drawable.ic_arrow_back_white_24dp)?.let { it }
-      backArrowBitmap?.let {
-        customTabsBuilder.setCloseButtonIcon(backArrowBitmap)
-      }
-      customTabsIntent.launchUrl(this, Uri.parse(recipe.url))
-    }
   }
 
   private fun loadRecipeImage(recipe: Recipe) {
@@ -106,23 +61,5 @@ class RecipeDetailActivity : AppCompatActivity(), Serializable {
 
         })
         .into(detail_recipe_image)
-  }
-
-  private fun setUpIngredients(recipe: Recipe) {
-    recipe.ingredientLines.forEach {
-
-      val ingredientLine = View.inflate(this, R.layout.ingredent_line, null)
-
-      val ingredientText = ingredientLine.findViewById<TextView>(R.id.detail_ingredient_line)
-
-      val bullet = getString(R.string.bullet)
-      val ingredientWithBullet = "$bullet $it"
-      ingredientText.text = ingredientWithBullet
-
-      val detailLayout: ViewGroup = findViewById(R.id.details_linear_layout)
-      // Ingredients are added at 2nd to last position in the layout
-      detailLayout.addView(ingredientText, 4,
-          LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
-    }
   }
 }
---
+++ app/src/main/java/com/zonkey/simplemealplanner/widgets/RecipeDetailCardView.kt
@@ -0,0 +1,153 @@
+package com.zonkey.simplemealplanner.widgets
+
+import android.content.Context
+import android.net.Uri
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.browser.customtabs.CustomTabsIntent.Builder
+import androidx.core.content.ContextCompat
+import com.zonkey.simplemealplanner.R
+import com.zonkey.simplemealplanner.R.color
+import com.zonkey.simplemealplanner.R.drawable
+import com.zonkey.simplemealplanner.R.layout
+import com.zonkey.simplemealplanner.R.string
+import com.zonkey.simplemealplanner.model.Recipe
+import com.zonkey.simplemealplanner.utils.createBitmapFromDrawableRes
+import kotlinx.android.synthetic.main.recipe_detail_card_view.view.detail_ingredients_label
+import kotlinx.android.synthetic.main.recipe_detail_card_view.view.detail_recipe_calories
+import kotlinx.android.synthetic.main.recipe_detail_card_view.view.detail_recipe_link
+import kotlinx.android.synthetic.main.recipe_detail_card_view.view.detail_recipe_servings
+import kotlinx.android.synthetic.main.recipe_detail_card_view.view.detail_recipe_source
+import kotlinx.android.synthetic.main.recipe_detail_card_view.view.detail_recipe_title
+import kotlin.math.roundToInt
+
+class RecipeDetailCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+  private var detailRecipeTitle: String? = null
+    set(value) {
+      field = value
+      detail_recipe_title.text = detailRecipeTitle
+    }
+
+  private var detailRecipeServing: String? = null
+    set(value) {
+      field = value
+      detail_recipe_servings.text = detailRecipeServing
+    }
+
+  private var detailRecipeCalories: String? = null
+    set(value) {
+      field = value
+      detail_recipe_calories.text = detailRecipeCalories
+    }
+
+  private var detailRecipeIngredientsLabel: String? = null
+    set(value) {
+      field = value
+      detail_ingredients_label.text = detailRecipeIngredientsLabel
+    }
+
+  private var detailRecipeSource: String? = null
+    set(value) {
+      field = value
+      detail_recipe_source.text = detailRecipeSource
+    }
+
+  private var detailRecipeLink: String? = null
+    set(value) {
+      field = value
+      detail_recipe_link.text = detailRecipeLink
+    }
+
+  private var detailRecipeButtonText: String? = null
+    set(value) {
+      field = value
+      detail_recipe_link.text = detailRecipeButtonText
+    }
+
+
+  init {
+    View.inflate(context, R.layout.recipe_detail_card_view, this)
+    orientation = LinearLayout.VERTICAL
+    setPadding(
+        context.resources.getDimensionPixelSize(R.dimen.common_margin_double),
+        context.resources.getDimensionPixelSize(R.dimen.common_margin_half),
+        context.resources.getDimensionPixelSize(R.dimen.common_margin_double),
+        0
+    )
+
+    attrs?.let {
+      context.obtainStyledAttributes(it, R.styleable.RecipeDetailCardView, 0, 0).apply {
+        detailRecipeTitle = getString(R.styleable.RecipeDetailCardView_title)
+        detailRecipeServing = getString(R.styleable.RecipeDetailCardView_serving)
+        detailRecipeCalories = getString(R.styleable.RecipeDetailCardView_calories)
+        detailRecipeCalories = getString(R.styleable.RecipeDetailCardView_calories)
+        detailRecipeIngredientsLabel = getString(R.styleable.RecipeDetailCardView_ingredients_label)
+        detailRecipeSource = getString(R.styleable.RecipeDetailCardView_source)
+        detailRecipeLink = getString(R.styleable.RecipeDetailCardView_link)
+        detailRecipeButtonText = getString(R.styleable.RecipeDetailCardView_button_text)
+        recycle()
+      }
+    }
+
+  }
+
+  fun setRecipeDetailCardItems(recipe: Recipe) {
+    val servingSizeText = "${context.getString(R.string.recipe_details_serving)} ${recipe.yield}"
+    detailRecipeServing = servingSizeText
+
+    val calsPerServing = (recipe.calories / recipe.yield).roundToInt()
+    val caloriesText = "${context.getString(R.string.recipe_detail_calories)} $calsPerServing"
+    detailRecipeCalories = caloriesText
+
+    val sourceLabel = context.getString(string.detail_recipe_source_label)
+    val sourceText = "$sourceLabel ${recipe.source}"
+    detailRecipeSource = sourceText
+
+    detailRecipeLink = recipe.url
+
+    detailRecipeButtonText = context.getString(string.detail_button_text)
+
+    setUpDetailsButton(recipe)
+
+    populateIngredients(recipe)
+  }
+
+  private fun setUpDetailsButton(recipe: Recipe) {
+    detail_recipe_link.setOnClickListener {
+      val customTabsBuilder = Builder()
+      val customTabsIntent = customTabsBuilder.build()
+      customTabsBuilder.setToolbarColor(ContextCompat.getColor(context, color.colorPrimary))
+      val backArrowBitmap = createBitmapFromDrawableRes(context,
+          drawable.ic_arrow_back_white_24dp)?.let { it }
+      backArrowBitmap?.let {
+        customTabsBuilder.setCloseButtonIcon(backArrowBitmap)
+      }
+      customTabsIntent.launchUrl(context, Uri.parse(recipe.url))
+    }
+  }
+
+  private fun populateIngredients(recipe: Recipe) {
+    recipe.ingredientLines.forEach {
+
+      val ingredientLine = View.inflate(context, layout.ingredent_line, null)
+
+      val ingredientText = ingredientLine.findViewById<TextView>(R.id.detail_ingredient_line)
+
+      val bullet = context.getString(string.bullet)
+      val ingredientWithBullet = "$bullet $it"
+      ingredientText.text = ingredientWithBullet
+
+      // Ingredients are added to the 4th position in the layout
+      this.addView(ingredientText, 4,
+          android.view.ViewGroup.LayoutParams(android.view.ViewGroup.LayoutParams.WRAP_CONTENT,
+              android.view.ViewGroup.LayoutParams.WRAP_CONTENT))
+    }
+  }
+}
