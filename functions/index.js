c458200908c36ef70848cced11532f425954554e
Nick Bradshaw <nickalasb@gmail.com>
2019-05-27 11:11:02 -0700

    SMP-17: Adding in dependencies for using firebase cloud functions and messaging
    * Added cloud function to send notification when a new recipe is added

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+//TODO change this use data instead of notification to have app handle background notifications
+// https://medium.com/@Miqubel/mastering-firebase-notifications-36a3ffe57c41
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendRecipeSharedNotification = functions.database.ref('/simple_meal_planner/users/{userId}/recipes/{recipeId}')
+.onWrite((change, context) => {
+
+  if (change.before.exists() || !change.after.exists()) {
+     return null;
+  }
+
+  const sharedRecipe = change.after;
+  console.log('Recipe = ', sharedRecipe.val());
+
+  const recipeName = sharedRecipe.child('label').val();
+  console.log('Recipe Name = ', recipeName);
+
+  const recipeImageUrl = sharedRecipe.child('image').val();
+  console.log('Recipe Image = ', recipeImageUrl);
+
+  const userWhoShared = sharedRecipe.child('sharedFromUser').val();
+  console.log('User who shared = ', userWhoShared);
+
+  const targetUserMessageTokenVal = admin.database().ref('/simple_meal_planner/users/' + context.params.userId + '/messagingToken')
+  .once('value', (snapshot) => {
+
+  const token = snapshot.val();
+  console.log('Target user token = ', token);
+
+    var message = {
+    	notification: {
+    		title: userWhoShared + ' has shared a recipe with you',
+    		body: 'Check out this recipe for ' + recipeName + '!',
+    		icon: recipeImageUrl
+    	}
+    };
+
+   admin.messaging().sendToDevice(token, message);
+  });
+
+  return Promise.all([targetUserMessageTokenVal]);
+});
